5e071195aa8a72a0b7087da7225cdc34f40ec09c
Mark Andrew Lov <drewlovendino17@gmail.com>
2018-03-19 02:33:37 +0800

    Updates

--- resources/views/root/reservations/pdf.blade.php
+++ resources/views/root/reservations/pdf.blade.php
@@ -6,55 +6,49 @@
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
         <title>{{ config('app.name') }}</title>
         <style type="text/css">
-            #invoice-title { font-size: 55px; font-weight: bold;}
-            #meta { margin-top: 0px; width: 400px; float: right; font-size: 45px;}
-            #right { margin-top: 100px; width: 400px; float: right; font-size: 20px;}   
-            #metas { margin-top: 1px; width: 100%; float: left; }
-            #metas1 { margin-top: 40px; width: 400px; float: right; }
-            #meta td { text-align: right;  }
-            #meta td.meta-head { text-align: left; background: #eee; }
-            #meta td textarea { width: 100%; height: 20px; text-align: right; }         
-            table {
-                border-collapse: collapse;
-                width: 100%;
-
-            }
-
-            th, td {
-                text-align: center;
-                padding: 18px;
-            }s
-
-            tr:nth-child(even){background-color: #f2f2f2}
-
-            th {
-                background-color: #0091EA;
-                color: white;
-            }
+        #invoice-title { font-size: 55px; font-weight: bold;}
+        #meta { margin-top: 0px; width: 400px; float: right; font-size: 45px;}
+        #right { margin-top: 100px; width: 400px; float: right; font-size: 20px;}
+        #metas { margin-top: 1px; width: 100%; float: left; }
+        #metas1 { margin-top: 40px; width: 400px; float: right; }
+        #meta td { text-align: right;  }
+        #meta td.meta-head { text-align: left; background: #eee; }
+        #meta td textarea { width: 100%; height: 20px; text-align: right; }
+        table {  
+        border-collapse: collapse;
+        width: 100%;
+        }
+        th, td {
+        text-align: center;
+        padding: 18px;
+        }s
+        tr:nth-child(even){background-color: #f2f2f2}
+        th {
+        background-color: #0091EA;
+        color: white;
+        }
         </style>
     </head>
     <body>
         <div id="status">
             Status:{{ $reservation->status }}
-        </div>                
+        </div>
         <div id="invoice-title">INVOICE
-        <div id="meta">
-            CARIBBEAN <br>
-            WATERPARK<br>
-            RESORT
-        </div>        
+            <div id="meta">
+                CARIBBEAN <br>
+                WATERPARK<br>
+                RESORT
+            </div>
         </div>
-
         <div id = "right">
-        {{ $reservation->creator->full_name }}, 80 Parang, Pulong Sampaloc
-        Doña Remedios Trinidad, Bulacan
+            {{ $reservation->creator->full_name }}, 80 Parang, Pulong Sampaloc
+            Doña Remedios Trinidad, Bulacan
         </div>
- 
- <br><br><br>
+        
+        <br><br><br>
         <table>
             <thead>
                 <tr>
-
                     <th>Created</th>
                     <th>Reference No</th>
                     <th>Invoice To</th>
@@ -65,11 +59,10 @@
                     <td>{{ Carbon::parse($reservation->created_at)->toFormattedDateString() }}</td>
                     <td>{{ $reservation->name }}</td>
                     <td>{{ $reservation->user->full_name }}</td>
-
                 </tr>
             </tbody>
-        </table>      
-<br><br>       
+        </table>
+        <br><br>
         <table>
             <thead>
                 <tr>
@@ -91,40 +84,30 @@
                 </tr>
                 @endforeach
             </tbody>
-         <br><br>   
+            <br><br>
         </table>
-                <table id="metas">
-                <tr>
-                    <th></th>
-                    <th>Reservation</th>
-                    <th></th>                   
-                </tr>
-                <tr>
-                    <td class="meta-head">Check-in date</td>
-                    <td>{{ Carbon::parse($reservation->checkin_date)->toFormattedDateString() }}</td>
-                    <td>TOTAL</td>
-                </tr>
-                <tr>
-
-                    <td class="meta-head">Check-out date:</td>
-                    <td>{{ Carbon::parse($reservation->checkout_date)->toFormattedDateString() }}</td>
-                    <td>{{ Helper::decimalFormat($reservation->price_payable) }}</td>
-                </tr>
-                <tr>
-                    <td class="meta-head">Days</td>
-                    <td>{{ $reservation->day_count }}</td>
-                    <td>*Taxes Included</td>
-                </tr>
-
-                </table>
-
-{{--         <div id = "metas1">
-        TOTAL<br><br><br>
-        {{ Helper::decimalFormat($reservation->price_payable) }}<br><br><br>
-        *Taxes Included
-        </div>         --}}
-
-            
-        </body>
-    </html>
- 
+        <table id="metas">
+            <tr>
+                <th></th>
+                <th>Reservation</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td class="meta-head">Check-in date</td>
+                <td>{{ Carbon::parse($reservation->checkin_date)->toFormattedDateString() }}</td>
+                <td>TOTAL</td>
+            </tr>
+            <tr>
+                <td class="meta-head">Check-out date:</td>
+                <td>{{ Carbon::parse($reservation->checkout_date)->toFormattedDateString() }}</td>
+                <td>{{ Helper::decimalFormat($reservation->price_payable) }}</td>
+            </tr>
+            <tr>
+                <td class="meta-head">Days</td>
+                <td>{{ $reservation->day_count }}</td>
+                <td>*Taxes Included</td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
