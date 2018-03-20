01ca8d9ae1ea5289588f94a4187f6ce501a45d55
Mark Andrew Lov <drewlovendino17@gmail.com>
2018-03-20 20:11:07 +0800

    Updates

--- app/Http/Controllers/Root/CouponsController.php
+++ app/Http/Controllers/Root/CouponsController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\Root;
 
+use App\Notifications\{ResourceCreated, ResourceUpdated, ResourceDeleted};
+use App\{Coupon, User};
+use ImageUploader;
+use File, URL;
+use Carbon, Image, Notify;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CouponsController extends Controller
 {
-    //
+    protected $superusers;
+
+    public function __construct()
+    {
+        $this->superusers = User::where('type', 'superuser')->get();
+    }
+
+    public function index()
+    {
+        $coupon = Coupon::all();
+
+        return view('root.coupons.index', ['coupons' => $coupon]);
+    }
+
+    
 }
